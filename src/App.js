5e59763d06037368064b4e87bab7a701bad532ce
banyuwangi1998 <atidhirabanyuwangi@gmail.com>
2021-12-16 19:38:56 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import { useSelector } from "react-redux";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
 
-function App() {
+const App = () => {
+  // const { userData } = useSelector((state) => state.auth)
+  const userData = null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            !userData ? <Navigate to="/login" /> : <Navigate to="/dashboard" />
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={!userData ? <Navigate to="/login" /> : <DashboardPage />}
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/reducers/auth.js
@@ -0,0 +1,14 @@
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case "LOGIN":
+      break;
+    case "REGISTER":
+      break;
+    case "LOGOUT":
+      break;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
